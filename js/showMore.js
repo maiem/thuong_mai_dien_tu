dd014d9f2b70cb7fa4ff8152382c42268eb6a7d7
Maianh0101 <hathimaianh03@gmail.com>
2021-03-30 02:36:10 +0700

    upload ver n

--- js/showMore.js
+++ js/showMore.js
@@ -1,9 +1,41 @@
-var numberOfItems=12;
+// $(document).ready(function () {
+//     // Load the first 4 list items from another HTML file
+//     //$('#myList').load('externalList.html li:lt(4)');
 
-$('#myList li:lt('+numberOfItems+')').show();
+//     $('#myList li:lt(12)').show();
+//     $('#showLess').hide();
 
-$('#loadMore').click(function () {
-    numberOfItems = numberOfItems+4;
-    $('#myList li:lt('+numberOfItems+')').show();
-    //$('#loadMore').css("display", "none");
+//     let items =  22;
+//     let shown =  4;
+
+//     $('#loadMore').click(function () {
+//         $('#loadMore').show();
+//         shown = $('#myList li:visible').size()+4;
+
+//         if(shown< items) {
+//             $('#myList li:lt('+shown+')').show();
+//         }
+//         else {
+//             $('#myList li:lt('+items+')').show();
+//              $('#loadMore').hide();
+//         }
+//     });
+
+//     $('#showLess').click(function () {
+//         $('#myList li').not(':lt(4)').hide();
+//     });
+// });
+
+
+
+$(document).ready(function () {
+    // Load the first 3 list items from another HTML file
+    //$('#myList').load('externalList.html li:lt(3)');
+    $('#myList li:lt(12)').show();
+    $('#loadMore').click(function () {
+        $('#myList li:lt(4)').show();
+    });
+    $('#showLess').click(function () {
+        $('#myList li').not(':lt(4)').hide();
+    });
 });
