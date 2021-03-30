0bf524feba96db250253f77e87bbd0fc460bcdc3
Maianh0101 <hathimaianh03@gmail.com>
2021-03-30 17:22:06 +0700

    ver nnn

---
+++ js/searchBar.js
@@ -0,0 +1,10 @@
+// $('search').on('click', function() {
+//     $('toggle-search-bar').toggle('display: inline-block');
+//   });
+
+$(document).ready(function() {
+	$(".search").click(function() {
+	   $(".toggle-search").toggle('visibility: visible','opacity: 1');
+	 });
+});
+
